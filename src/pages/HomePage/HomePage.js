53fa5a1522bf17ed0d8ddeeda87dbb55aada049f
navayuvan-avasoft <navayuvan.s@avasoft.com>
2021-12-10 21:49:56 +0530

    Setup initial project

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = function() {
+  return (
+    <div id="HomePage">
+      <h1>Application is under development, please come back later!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
