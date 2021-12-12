92935b8c4faec87b6b86953ee6da70270cf9a046
navayuvan-avasoft <navayuvan.s@avasoft.com>
2021-12-12 10:17:55 +0530

    Add support for SASS compilation

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./HomePage.css";
+import React from 'react';
+import './HomePage.scss';
 
-const HomePage = function() {
+const HomePage = function () {
   return (
     <div id="HomePage">
       <h1>Application is under development, please come back later!</h1>
     </div>
   );
-}
+};
 
 export default HomePage;
