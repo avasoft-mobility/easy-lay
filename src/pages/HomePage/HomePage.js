7558971b9b8a4f872a581e4862828108ec706126
navayuvan-avasoft <navayuvan.s@avasoft.com>
2021-12-12 16:24:19 +0530

    Configure eslint for arrow-functions-only mode

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './HomePage.scss';
 
-const HomePage = function () {
-  return (
-    <div id="HomePage">
-      <h1>Application is under development, please come back later!</h1>
-    </div>
-  );
-};
+const HomePage = () => (
+  <div id="HomePage">
+    <h1>Application is under development, please come back later!</h1>
+  </div>
+);
 
 export default HomePage;
